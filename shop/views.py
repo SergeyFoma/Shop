8a79b0ce3d1cd7fba367d3050e2afa6eb2a9ba34
SergeyFoma <asdfg2105c@gmail.com>
2024-10-29 07:29:32 +0500

    'sdf'

--- shop/views.py
+++ shop/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from goods.models import Categories
 #from PIL import Image
 
+
 def index(request):
-	categories=Categories.objects.all()
+	#categories=Categories.objects.all()
 	context={
-		'categories':categories,
+		#'categories':categories,
 	}
 	return render(request, 'shop/index.html', context)
 
