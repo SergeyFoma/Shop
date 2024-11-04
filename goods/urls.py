45999a120f85937bde3e0186851fcaa008df458e
SergeyFoma <asdfg2105c@gmail.com>
2024-11-04 15:35:51 +0500

    search

--- goods/urls.py
+++ goods/urls.py
@@ -4,7 +4,7 @@ from goods import views
 app_name='goods'
 
 urlpatterns=[
+    path('search/', views.catalog, name='search'),
 	path('<slug:category_slug>/', views.catalog, name='index'),
-    #path('<slug:category_slug>/<int:page>/', views.catalog, name='index'),
 	path('products/<slug:product_slug>/', views.products, name='products'),
 ]
